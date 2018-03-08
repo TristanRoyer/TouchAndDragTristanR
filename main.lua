73a42102b7c62565a807bb687e6383d0adc55e10
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-03-07 21:01:59 -0500

    edited code

--- main.lua
+++ main.lua
@@ -11,17 +11,18 @@ display.setStatusBar(display.HiddenStatusBar)
 local backgroundImage = display.newImageRect("Images/background.png", 2048,1536)
 
 --displays the Yellow Girl
-local yellowGirl = display.newImageRect("Images/yellowGirl.png",150,150)
-local yellowGirlWidth = yellowGirl.width 
-local yellowGirlHeight = yellowGirl.height
+local yellowGirl = display.newImageRect("Images/yellowGirl.png",140,150)
+local yellowGirlWidth = yellowGirl.Width 
+local yellowGirlHeight = yellowGirl.Height
 
 -- displays the Blue Girl
 local blueGirl = display.newImageRect("Images/blueGirl.png", 150,180)
-local blueGirlWidth = blueGirl.width
-local blueGirlHeight = blueGirl.height
+local blueGirlWidth = blueGirl.Width
+local blueGirlHeight = blueGirl.Height
+
 
 --my boolean variables to keep track of which object I touched first
-local alreadyTouchedYellowGirl = false
+local alreadyTouchedyellowGirl = false
 local alreadyTouchedBlueGirl = false
 
 --set the initial x and y position of myImage
@@ -37,8 +38,8 @@ blueGirl.y = 400
 -- Description: when blue girl is touched, move her
 local function BlueGirlListener(touch)
 
-   if(touch.phase == "Began") then
-    if (alreadyTouchedYellowGirl == false ) then 
+   if(touch.phase == "began") then
+    if (alreadyTouchedyellowGirl == false ) then 
       alreadyTouchedBlueGirl = true
      end 
    end
@@ -50,7 +51,7 @@ local function BlueGirlListener(touch)
 
   if (touch.phase == "ended") then 
      alreadyTouchedBlueGirl = false
-     alreadyTouchedYellowGirl = false
+     alreadyTouchedyellowGirl = false
   end 
 end
 
@@ -59,16 +60,26 @@ end
 -- Input: touch listener
 -- output: none
 -- Description: when blue girl is touched, move her
-local function BlueGirlListener(touch)
-if(touch.phase == "began")	then
+local function yellowGirlListener(touch)
+if (touch.phase == "began")	then
 	if (alreadytouchedBlueGirl == false ) then
-	 (alreadyTouchedBlueGirl == false ) 
-	end
-end
+	 alreadyTouchedyellowGirl = true 
+	 end
+    end
 
-if( (touch.phase == ))
+   if( (touch.phase == "moved") and (alreadyTouchedyellowGirl == true) ) then 
+	yellowGirl.x = touch.x
+	yellowGirl.y = touch.y
+   end
+
+  if (touch.phase == "ended") then
+    alreadyTouchedBlueGirl = false
+    alreadyTouchedyellowGirl = false
+  end
+end
 
 
 --add the respective listeners to each object
 blueGirl:addEventListener("touch", BlueGirlListener)
+yellowGirl:addEventListener("touch", yellowGirlListener)
 
